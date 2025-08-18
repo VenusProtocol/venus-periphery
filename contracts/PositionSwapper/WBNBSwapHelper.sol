2853954cd09a4daaf33ffcd1c87ef5e40d30cabd
web3rover <arsenal.narayan@gmail.com>
2025-08-18 12:05:24 +0530

    fix: use forceApprove

--- contracts/PositionSwapper/WBNBSwapHelper.sol
+++ contracts/PositionSwapper/WBNBSwapHelper.sol
@@ -70,8 +70,7 @@ contract WBNBSwapHelper is ISwapHelper {
             emit SwappedToWBNB(amount);
         } else {
             IERC20Upgradeable(WBNB).safeTransferFrom(msg.sender, address(this), amount);
-            IERC20Upgradeable(address(WBNB)).safeApprove(address(WBNB), 0);
-            IERC20Upgradeable(address(WBNB)).safeApprove(address(WBNB), amount);
+            IERC20Upgradeable(address(WBNB)).forceApprove(address(WBNB), amount);
             WBNB.withdraw(amount);
             payable(msg.sender).transfer(amount);
             emit SwappedToBNB(amount);
