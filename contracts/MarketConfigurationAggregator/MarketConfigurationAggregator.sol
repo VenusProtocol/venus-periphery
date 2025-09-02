e4d0ece24972532f5d48fd7a1bfaab43c4a0e487
GitGuru7 <aditya@venus.io>
2025-09-02 11:22:10 +0530

    fix: lint

--- contracts/MarketConfigurationAggregator/MarketConfigurationAggregator.sol
+++ contracts/MarketConfigurationAggregator/MarketConfigurationAggregator.sol
@@ -29,7 +29,7 @@ contract MarketConfigurationAggregator {
         bool borrowAllowed;
     }
 
-    /// @notice The Comptroller contract 
+    /// @notice The Comptroller contract
     IComptroller public immutable COMPTROLLER;
 
     /**
