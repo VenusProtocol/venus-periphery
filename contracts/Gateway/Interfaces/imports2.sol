9f8dd2fd8e94598e0b03561686073c3ad874012e
Debugger022 <debugger022@gmail.com>
2025-09-11 20:05:49 +0530

    feat: add imports for tests

---
+++ contracts/Gateway/Interfaces/imports2.sol
@@ -0,0 +1,5 @@
+pragma solidity ^0.8.0;
+
+import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import { MockToken } from "@venusprotocol/venus-protocol/contracts/test/MockToken.sol";
+import { IAccessControlManagerV8 } from "@venusprotocol/governance-contracts/contracts/Governance/IAccessControlManagerV8.sol";
