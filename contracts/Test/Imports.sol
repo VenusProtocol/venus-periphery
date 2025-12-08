1e4da12673c1a3dbb7dfe3b01f061fcaf3ec28cb
web3rover <arsenal.narayan@gmail.com>
2025-12-08 23:25:00 +0530

    fix: added fork tests for deviation logic

---
+++ contracts/Test/Imports.sol
@@ -0,0 +1,4 @@
+pragma solidity ^0.8.25;
+
+import { ChainlinkOracle } from "@venusprotocol/oracle/contracts/oracles/ChainlinkOracle.sol";
+import { ResilientOracle } from "@venusprotocol/oracle/contracts/ResilientOracle.sol";
--- tests/hardhat/Fork/PriceDeviationSentinel.ts
+++ tests/hardhat/Fork/PriceDeviationSentinel.ts
@@ -2,13 +2,18 @@ import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { expect } from "chai";
 import { parseUnits } from "ethers/lib/utils";
-import { ethers } from "hardhat";
+import { ethers, upgrades } from "hardhat";
 
 import {
+  ChainlinkOracle,
+  ChainlinkOracle__factory,
+  ComptrollerMock,
   ComptrollerMock__factory,
   IAccessControlManagerV8,
   IAccessControlManagerV8__factory,
   PriceDeviationSentinel,
+  ResilientOracle,
+  ResilientOracle__factory,
 } from "../../../typechain";
 import { forking, initMainnetUser } from "./utils";
 
@@ -19,17 +24,25 @@ const TRX = "0xCE7de646e7208a4Ef112cb6ed5038FA6cC6b12e3";
 const BTCB = "0x7130d2A12B9BCbFAe4f2634d864A1Ee1Ce3Ead9c";
 const ORACLE = "0x6592b5DE802159F3E74B2486b091D11a8256ab8A";
 const USDT = "0x55d398326f99059fF775485246999027B3197955";
+const CHAINLINK_ORACLE = "0x1B2103441A0A108daD8848D8F5d790e4D402921F";
+const WBNB = "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c";
+const vWBNB = "0x6bCa74586218dB34cdB402295796b79663d816e9";
 
 const FORK_MAINNET = process.env.FORKED_NETWORK === "bscmainnet";
 
 type SetupMarketFixture = {
   timelock: SignerWithAddress;
   priceDeviationSentinel: PriceDeviationSentinel;
+  coreComptroller: ComptrollerMock;
+  chainlinkOracle: ChainlinkOracle;
+  resilientOracle: ResilientOracle;
 };
 
 const setupMarketFixture = async (): Promise<SetupMarketFixture> => {
   const timelock = await initMainnetUser(NORMAL_TIMELOCK, ethers.utils.parseUnits("2"));
   const coreComptroller = ComptrollerMock__factory.connect(COMPTROLLER, timelock);
+  const chainlinkOracle = ChainlinkOracle__factory.connect(CHAINLINK_ORACLE, timelock);
+  const resilientOracle = ResilientOracle__factory.connect(ORACLE, timelock);
 
   const priceDeviationSentinelFactory = await ethers.getContractFactory("PriceDeviationSentinel");
   const priceDeviationSentinel = await upgrades.deployProxy(priceDeviationSentinelFactory, [ACM], {
@@ -60,10 +73,18 @@ const setupMarketFixture = async (): Promise<SetupMarketFixture> => {
       "setTokenConfig(address,(uint8,uint8,address))",
       NORMAL_TIMELOCK,
     );
+  await acm
+    .connect(timelock)
+    .giveCallPermission(priceDeviationSentinel.address, "setTrustedKeeper(address,bool)", NORMAL_TIMELOCK);
+
+  await priceDeviationSentinel.connect(timelock).setTrustedKeeper(timelock.address, true);
 
   return {
     timelock,
     priceDeviationSentinel,
+    coreComptroller,
+    chainlinkOracle,
+    resilientOracle,
   };
 };
 
@@ -73,11 +94,15 @@ if (FORK_MAINNET) {
   forking(blockNumber, () => {
     let priceDeviationSentinel: PriceDeviationSentinel;
     let timelock: SignerWithAddress;
+    let coreComptroller: ComptrollerMock;
+    let chainlinkOracle: ChainlinkOracle;
+    let resilientOracle: ResilientOracle;
 
     describe("PriceDeviationSentinel", () => {
       beforeEach(async () => {
-        ({ priceDeviationSentinel, timelock } = await loadFixture(setupMarketFixture));
-        // setTokenConfig for TRX
+        ({ priceDeviationSentinel, timelock, coreComptroller, chainlinkOracle, resilientOracle } = await loadFixture(
+          setupMarketFixture,
+        ));
         await priceDeviationSentinel.connect(timelock).setTokenConfig(TRX, {
           deviation: 10,
           dex: 1,
@@ -111,6 +136,64 @@ if (FORK_MAINNET) {
         it("check BTCB price", async () => {
           const price = await priceDeviationSentinel["getDexPrice(address)"](BTCB);
           expect(price).to.be.equal(parseUnits("91784.949423700465674501", 18));
+        });
+      });
+
+      describe("check price deviation", () => {
+        beforeEach(async () => {
+          await priceDeviationSentinel.connect(timelock).setTokenConfig(WBNB, {
+            deviation: 10,
+            dex: 0,
+            pool: "0x28dF0835942396B7a1b7aE1cd068728E6ddBbAfD",
+          });
+
+          await resilientOracle.connect(timelock).setTokenConfig({
+            asset: WBNB,
+            oracles: [CHAINLINK_ORACLE, ethers.constants.AddressZero, ethers.constants.AddressZero],
+            enableFlagsForOracles: [true, false, false],
+            cachingEnabled: false,
+          });
+        });
+
+        it("WBNB dex price lower than chainlink price", async () => {
+          await chainlinkOracle.connect(timelock).setDirectPrice(WBNB, parseUnits("3000", 18));
+
+          let isDeviated = await priceDeviationSentinel.checkPriceDeviation(vWBNB);
+          expect(isDeviated.hasDeviation).to.be.equal(true);
+
+          await priceDeviationSentinel.connect(timelock).handleDeviation(vWBNB);
+
+          let data = await coreComptroller.poolMarkets(0, vWBNB);
+          expect(data.collateralFactorMantissa).to.be.equal(0);
+
+          await chainlinkOracle.connect(timelock).setDirectPrice(WBNB, parseUnits("900", 18));
+
+          isDeviated = await priceDeviationSentinel.checkPriceDeviation(vWBNB);
+          expect(isDeviated.hasDeviation).to.be.equal(false);
+
+          await priceDeviationSentinel.connect(timelock).handleDeviation(vWBNB);
+
+          data = await coreComptroller.poolMarkets(0, vWBNB);
+          expect(data.collateralFactorMantissa).to.be.not.equal(0);
+        });
+
+        it("WBNB dex price higher than chainlink price", async () => {
+          await chainlinkOracle.connect(timelock).setDirectPrice(WBNB, parseUnits("300", 18));
+
+          const isDeviated = await priceDeviationSentinel.checkPriceDeviation(vWBNB);
+          expect(isDeviated.hasDeviation).to.be.equal(true);
+
+          await priceDeviationSentinel.connect(timelock).handleDeviation(vWBNB);
+
+          let isPaused = await coreComptroller.actionPaused(vWBNB, 2);
+          expect(isPaused).to.be.equal(true);
+
+          await chainlinkOracle.connect(timelock).setDirectPrice(WBNB, parseUnits("900", 18));
+
+          await priceDeviationSentinel.connect(timelock).handleDeviation(vWBNB);
+
+          isPaused = await coreComptroller.actionPaused(vWBNB, 2);
+          expect(isPaused).to.be.equal(false);
         });
       });
     });
