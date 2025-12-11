2f898bd7d5d21600932e201fddb3c972321b01e0
web3rover <arsenal.narayan@gmail.com>
2025-12-11 10:19:35 +0530

    Revert "fix: move oracles to oracle repo"
    This reverts commit cffb86f8ef93832e38c823e38ee4a268f472fab4.

--- contracts/Test/Imports.sol
+++ contracts/Test/Imports.sol
@@ -2,6 +2,3 @@ pragma solidity ^0.8.25;
 
 import { ChainlinkOracle } from "@venusprotocol/oracle/contracts/oracles/ChainlinkOracle.sol";
 import { ResilientOracle } from "@venusprotocol/oracle/contracts/ResilientOracle.sol";
-import { SentinelOracle } from "@venusprotocol/oracle/contracts/SentinelOracle.sol";
-import { UniswapOracle } from "@venusprotocol/oracle/contracts/oracles/UniswapOracle.sol";
-import { PancakeSwapOracle } from "@venusprotocol/oracle/contracts/oracles/PancakeSwapOracle.sol";
