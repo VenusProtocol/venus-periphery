cb19bbc4350390731b3a34e4bd90e658eaafe913
web3rover <arsenal.narayan@gmail.com>
2025-12-11 20:17:53 +0530

    fix: fixed e2e tests

--- contracts/Test/Imports.sol
+++ contracts/Test/Imports.sol
@@ -2,3 +2,5 @@ pragma solidity ^0.8.25;
 
 import { ChainlinkOracle } from "@venusprotocol/oracle/contracts/oracles/ChainlinkOracle.sol";
 import { ResilientOracle } from "@venusprotocol/oracle/contracts/ResilientOracle.sol";
+import { IAccessControlManagerV8 } from "@venusprotocol/governance-contracts/contracts/Governance/IAccessControlManagerV8.sol";
+import { ComptrollerMock } from "@venusprotocol/venus-protocol/contracts/test/ComptrollerMock.sol";
