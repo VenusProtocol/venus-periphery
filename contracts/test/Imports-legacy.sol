db59423972654f3b48e2802c8c28258bc72e1b5c
web3rover <arsenal.narayan@gmail.com>
2025-12-16 15:31:41 +0530

    fix: remove duplicate imports

--- contracts/test/Imports-legacy.sol
+++ contracts/test/Imports-legacy.sol
@@ -1,4 +1,3 @@
-import { InterestRateModelHarness } from "@venusprotocol/venus-protocol/contracts/test/InterestRateModelHarness.sol";
 // SPDX-License-Identifier: BSD-3-Clause
 pragma solidity 0.5.16;
 
--- contracts/test/Imports.sol
+++
@@ -1,6 +0,0 @@
-pragma solidity ^0.8.25;
-
-import { ChainlinkOracle } from "@venusprotocol/oracle/contracts/oracles/ChainlinkOracle.sol";
-import { ResilientOracle } from "@venusprotocol/oracle/contracts/ResilientOracle.sol";
-import { IAccessControlManagerV8 } from "@venusprotocol/governance-contracts/contracts/Governance/IAccessControlManagerV8.sol";
-import { ComptrollerMock } from "@venusprotocol/venus-protocol/contracts/test/ComptrollerMock.sol";
