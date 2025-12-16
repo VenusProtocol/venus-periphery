ea31a656ae8ccadb794f0aefe31671c8f9ecf500
web3rover <arsenal.narayan@gmail.com>
2025-12-16 15:11:43 +0530

    fix: added imports

--- contracts/test/Imports.sol
+++ contracts/test/Imports.sol
@@ -1,10 +1,6 @@
-// SPDX-License-Identifier: BSD-3-Clause
-pragma solidity 0.8.25;
+pragma solidity ^0.8.25;
 
+import { ChainlinkOracle } from "@venusprotocol/oracle/contracts/oracles/ChainlinkOracle.sol";
+import { ResilientOracle } from "@venusprotocol/oracle/contracts/ResilientOracle.sol";
 import { IAccessControlManagerV8 } from "@venusprotocol/governance-contracts/contracts/Governance/IAccessControlManagerV8.sol";
 import { ComptrollerMock } from "@venusprotocol/venus-protocol/contracts/test/ComptrollerMock.sol";
-import { VBNB } from "@venusprotocol/venus-protocol/contracts/Tokens/VTokens/VBNB.sol";
-import { Diamond } from "@venusprotocol/venus-protocol/contracts/Comptroller/Diamond/Diamond.sol";
-import { MockVBNB } from "@venusprotocol/venus-protocol/contracts/test/MockVBNB.sol";
-import { VBep20Harness } from "@venusprotocol/venus-protocol/contracts/test/VBep20Harness.sol";
-import { ComptrollerLens } from "@venusprotocol/venus-protocol/contracts/Lens/ComptrollerLens.sol";
