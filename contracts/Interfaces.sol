1db16853286db642c3e08da6138bc9c741803651
Debugger022 <debugger022@gmail.com>
2026-02-23 15:20:31 +0530

    feat: add barrel Interfaces.sol to re-export split interface files
    Prevents duplicate artifact errors (HH701) by making Interfaces.sol
    a re-export file pointing to contracts/Interfaces/*.sol, preserving
    the import path used by SwapRouter without requiring redeployment.

---
+++ contracts/Interfaces.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: BSD-3-Clause
+pragma solidity ^0.8.25;
+
+import { IVToken } from "./Interfaces/IVToken.sol";
+import { IComptroller } from "./Interfaces/IComptroller.sol";
+import { IVBNB } from "./Interfaces/IVBNB.sol";
+import { IWBNB } from "./Interfaces/IWBNB.sol";
+import { IFlashLoanReceiver } from "./Interfaces/IFlashLoanReceiver.sol";
+import { IProtocolShareReserve } from "./Interfaces/IProtocolShareReserve.sol";
